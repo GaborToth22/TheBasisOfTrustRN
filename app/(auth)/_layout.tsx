e2aa9b7ba112b61c193c01e5b93b5f4c569cd623
GaborToth22 <toccs22@gmail.com>
2024-06-20 15:37:06 +0200

    feat: add _layout for auth

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,31 @@
+import { View, Text } from 'react-native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+const AuthLayout = () => {
+    return (
+        <>
+            <Stack>
+                <Stack.Screen 
+                    name='login'
+                    options={{
+                        headerShown: false
+                    }}
+                />
+                <Stack.Screen 
+                    name='register'
+                    options={{
+                        headerShown: false
+                    }}
+                />
+            </Stack>
+
+            <StatusBar 
+                backgroundColor="#161622"
+                style="light"
+            />
+        </>
+    )
+}
+
+export default AuthLayout;
