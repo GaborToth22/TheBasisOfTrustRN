796eb220bcea9c71b252fbd2a0f1a7b37716638a
GaborToth22 <toccs22@gmail.com>
2024-06-25 13:52:48 +0200

    feat: wrap the Rootlayout with the LoggedUserProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,13 @@
 import { Stack, Slot } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
-
+import { LoggedUserProvider } from '../context/globalContext'
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false}} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false}} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
-    </Stack>
+    <LoggedUserProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false}} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false}} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
+      </Stack>
+    </LoggedUserProvider>
   );
 }
