dd09dc425ebba914e1513760b5386887f5a250c1
GaborToth22 <toccs22@gmail.com>
2024-07-10 14:07:23 +0200

    feat: add Header and render the balances

--- app/(tabs)/dashboard.jsx
+++ app/(tabs)/dashboard.jsx
@@ -1,7 +1,9 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView, ScrollView, } from 'react-native';
 import React, { useState, useEffect} from 'react';
 import tw from 'twrnc';
 import { useGlobalState } from '../../context/globalContext';
+import Header from '../../components/Header';
+import { Colors } from "../../constants/Colors";
 
 const Dashboard = () => {
     const { loggedUser, setLoggedUser } = useGlobalState();
@@ -16,6 +18,7 @@ const Dashboard = () => {
     }, [loggedUser]);
 
     console.log(loggedUser)
+
     const fetchBalances = async () => {
         try {
             const response = await fetch(`http://192.168.1.8:5263/balance/${loggedUser.id}`);
@@ -63,13 +66,91 @@ const Dashboard = () => {
         }
     }, [balances, loggedUser]);
 
+    const getTextColor = (value) => {
+        if (value > 0) {
+            return { color: Colors.tbot.positive };
+        } else if (value < 0) {
+            return { color: Colors.tbot.negative };
+        } else {
+            return { color: Colors.tbot.white };
+        }
+    };
+
+    function renderYouOwe(userBalances) {
+        const friendshipsSent = loggedUser.friendshipsSent || [];
+        const friendshipsReceived = loggedUser.friendshipsReceived || [];
+        const friendships = friendshipsSent.concat(friendshipsReceived);
+      return(
+        <>
+            {Object.entries(userBalances).map(([userId, balance]) => {
+                if (balance < 0) {
+                    const friend = friendships.find(friendship => friendship.receiverId == userId || friendship.senderId == userId);
+                    const friendName = friend.receiverId == userId ? friend.receiverName : friend.senderName;
+                    return (
+                        <View key={userId} style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text style={tw`text-lg text-white mr-2`}>{friendName} </Text><Text style={[tw`text-lg`, getTextColor(parseFloat(youOwe))]}>{balance}</Text>
+                        </View>
+                    );
+                } else {
+                    return null;
+                }
+            })}
+        </>
+      )        
+    }
+
+    function renderOwesYou(userBalances) {
+        const friendships = loggedUser.friendshipsSent.concat(loggedUser.friendshipsReceived);
+        return(
+          <>
+              {Object.entries(userBalances).map(([userId, balance]) => {
+                  if (balance > 0) {
+                      const friend = friendships.find(friendship => friendship.receiverId == userId || friendship.senderId == userId);
+                      const friendName = friend.receiverId == userId ? friend.receiverName : friend.senderName;
+                      return (
+                        <View key={userId} style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text style={tw`text-lg text-white mr-2`}>{friendName} </Text><Text style={[tw`text-lg`, getTextColor(parseFloat(owesYou))]}>{balance}</Text>
+                        </View>
+                      );
+                  } else {
+                      return null;
+                  }
+              })}
+          </>
+        )        
+      }
     console.log(balances);
     console.log(total);
     console.log(youOwe);
     console.log(owesYou);
+
     return (
-        <View style={tw`flex-1 justify-center`}>
-            <Text style={tw`text-2xl font-bold text-center`}>Dashboard Screen is coming soon</Text>
+        <View style={[tw`flex-1`, { backgroundColor: Colors.tbot.bg}]}>
+            <Header username={loggedUser.username}/>
+            <ScrollView contentContainerStyle={[tw`flex-grow`, { backgroundColor: Colors.tbot.secondary}]}>
+                <View style={ tw`p-3 `}>
+                    <Text style={tw`text-2xl text-center text-white`}>Total Balance</Text>
+                    <Text style={[tw`text-2xl text-center`, getTextColor(parseFloat(total))]}>{total}</Text>
+                </View>
+                <View style={{ padding: 5, flexDirection: 'row', justifyContent: 'space-between', borderBottomColor: 'black', borderBottomWidth: 1}}>
+                    <View>
+                        <Text style={tw`text-2xl text-center text-white`}>You Owe</Text>
+                        <Text style={[tw`text-2xl text-center`, getTextColor(parseFloat(youOwe))]}>{youOwe}</Text>
+                    </View>
+                    <View>
+                        <Text style={tw`text-2xl text-center text-white`}>Owes You</Text>
+                        <Text style={[tw`text-2xl text-center`, getTextColor(parseFloat(owesYou))]}>{owesYou}</Text>
+                    </View>
+                </View>
+                <View style={{ padding: 5, flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <View>
+                        {renderYouOwe(userBalances)}
+                    </View>
+                    <View>
+                        {renderOwesYou(userBalances)}
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     )
 }
