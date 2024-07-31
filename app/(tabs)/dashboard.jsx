dff03f24217fff943df82f62a8029752007c8789
GaborToth22 <toccs22@gmail.com>
2024-07-31 13:59:18 +0200

    refactor: outsource the styles to stylesheet

--- app/(tabs)/dashboard.jsx
+++ app/(tabs)/dashboard.jsx
@@ -1,12 +1,12 @@
-import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
-import React, { useState, useEffect} from 'react';
+import { View, Text, ScrollView, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import tw from 'twrnc';
 import { useGlobalState } from '../../context/globalContext';
 import Header from '../../components/Header';
 import { Colors } from "../../constants/Colors";
 import AddExpenseModal from '../../components/AddExpenseModal';
 import SettleUpModal from '../../components/SettleUpModal';
-import tbot from '../../constants/TBOT.png'
+import tbot from '../../constants/TBOT.png';
 import { useFocusEffect } from '@react-navigation/native';
 
 const Dashboard = () => {
@@ -20,17 +20,21 @@ const Dashboard = () => {
     const [settleUpModalShow, setSettleUpModalShow] = useState(false);
 
     useEffect(() => {
-        fetchBalances();
+        if (loggedUser) {
+            fetchBalances();
+        }
     }, [loggedUser, addExpenseModalShow, settleUpModalShow]);
 
     useFocusEffect(
         React.useCallback(() => {
-          fetchBalances();
+            if (loggedUser) {
+                fetchBalances();
+            }
         }, [])
-      );
+    );
 
-    console.log(loggedUser)
-    console.log(addExpenseModalShow)
+    console.log(loggedUser);
+    console.log(addExpenseModalShow);
 
     const fetchBalances = async () => {
         try {
@@ -66,12 +70,13 @@ const Dashboard = () => {
 
             balances.forEach(balance => {
                 const amount = balance.amount;                
-                console.log(balance.userId)
+                console.log(balance.userId);
                 if (balance.userId === loggedUser.id) {
                     userBalances[balance.participantUserId] = (userBalances[balance.participantUserId] || 0) + amount;
                 } else {
                     userBalances[balance.userId] = (userBalances[balance.userId] || 0) - amount;
-                }})
+                }
+            });
 
             Object.values(userBalances).forEach(balance => {
                 if (balance < 0) {
@@ -81,14 +86,14 @@ const Dashboard = () => {
                 }
             });
             const formattedBalances = {};
-        Object.keys(userBalances).forEach(key => {
-            formattedBalances[key] = userBalances[key].toFixed(2);
-        });
+            Object.keys(userBalances).forEach(key => {
+                formattedBalances[key] = userBalances[key].toFixed(2);
+            });
 
-        setYouOwe(youOweSum.toFixed(2));
-        setOwesYou(owesYouSum.toFixed(2));
-        setTotal((owesYouSum + youOweSum).toFixed(2));
-        setUserBalances(formattedBalances);
+            setYouOwe(youOweSum.toFixed(2));
+            setOwesYou(owesYouSum.toFixed(2));
+            setTotal((owesYouSum + youOweSum).toFixed(2));
+            setUserBalances(formattedBalances);
         }
     }, [balances, loggedUser]);
 
@@ -106,86 +111,88 @@ const Dashboard = () => {
         const friendshipsSent = loggedUser.friendshipsSent || [];
         const friendshipsReceived = loggedUser.friendshipsReceived || [];
         const friendships = friendshipsSent.concat(friendshipsReceived);
-      return(
-        <>
-            {Object.entries(userBalances).map(([userId, balance]) => {
-                if (balance < 0) {
-                    const friend = friendships.find(friendship => friendship.receiverId == userId || friendship.senderId == userId);
-                    const friendName = friend.receiverId == userId ? friend.receiverName : friend.senderName;
-                    return (
-                        <View key={userId} style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <Text style={tw`text-lg text-white mr-2`}>{friendName} </Text><Text style={[tw`text-lg`, getTextColor(parseFloat(youOwe))]}>{balance}</Text>
-                        </View>
-                    );
-                } else {
-                    return null;
-                }
-            })}
-        </>
-      )        
+        return(
+            <>
+                {Object.entries(userBalances).map(([userId, balance]) => {
+                    if (balance < 0) {
+                        const friend = friendships.find(friendship => friendship.receiverId == userId || friendship.senderId == userId);
+                        const friendName = friend.receiverId == userId ? friend.receiverName : friend.senderName;
+                        return (
+                            <View key={userId} style={styles.balanceRow}>
+                                <Text style={styles.balanceText}>{friendName} </Text>
+                                <Text style={[styles.balanceText, getTextColor(parseFloat(youOwe))]}>{balance}</Text>
+                            </View>
+                        );
+                    } else {
+                        return null;
+                    }
+                })}
+            </>
+        );        
     }
 
     function renderOwesYou(userBalances) {
         const friendships = loggedUser.friendshipsSent.concat(loggedUser.friendshipsReceived);
         return(
-          <>
-              {Object.entries(userBalances).map(([userId, balance]) => {
-                  if (balance > 0) {
-                      const friend = friendships.find(friendship => friendship?.receiverId == userId || friendship?.senderId == userId);
-                      const friendName = friend?.receiverId == userId ? friend?.receiverName : friend?.senderName;
-                      return (
-                        <View key={userId} style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <Text style={tw`text-lg text-white mr-2`}>{friendName} </Text><Text style={[tw`text-lg`, getTextColor(parseFloat(owesYou))]}>{balance}</Text>
-                        </View>
-                      );
-                  } else {
-                      return null;
-                  }
-              })}
-          </>
-        )        
-      }
+            <>
+                {Object.entries(userBalances).map(([userId, balance]) => {
+                    if (balance > 0) {
+                        const friend = friendships.find(friendship => friendship?.receiverId == userId || friendship?.senderId == userId);
+                        const friendName = friend?.receiverId == userId ? friend?.receiverName : friend?.senderName;
+                        return (
+                            <View key={userId} style={styles.balanceRow}>
+                                <Text style={styles.balanceText}>{friendName} </Text>
+                                <Text style={[styles.balanceText, getTextColor(parseFloat(owesYou))]}>{balance}</Text>
+                            </View>
+                        );
+                    } else {
+                        return null;
+                    }
+                })}
+            </>
+        );        
+    }
 
     return (
-        <View style={[tw`flex-1`, { backgroundColor: Colors.tbot.bg}]}>
+        <View style={styles.container}>
             <Header username={loggedUser.username}/>
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <TouchableOpacity
-                style={[tw`rounded-xl min-h-[62px] min-w-[40%] justify-center items-center m-2`, { backgroundColor: 'orange' }]}        
-                onPress={() => setAddExpenseModalShow(true)}
-                activeOpacity={0.7}
-            >
-                <Text style={[tw`font-bold text-base`]}>Add Expense</Text>
-            </TouchableOpacity>
-            <Image
-            source={tbot}
-            style={[tw`w-[40px] h-[40px]`]}                        
-            resizeMode='contain'
-            />
-            <TouchableOpacity
-                style={[tw`rounded-xl min-h-[62px] min-w-[40%] justify-center items-center m-2`, { backgroundColor: Colors.tbot.green }]}        
-                onPress={() => setSettleUpModalShow(true)}
-                activeOpacity={0.7}
-            >               
-                <Text style={[tw`font-bold text-base`]}>Settle Up</Text>
-            </TouchableOpacity>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    style={[styles.button, { backgroundColor: 'orange' }]}        
+                    onPress={() => setAddExpenseModalShow(true)}
+                    activeOpacity={0.7}
+                >
+                    <Text style={styles.buttonText}>Add Expense</Text>
+                </TouchableOpacity>
+                <Image
+                    source={tbot}
+                    style={styles.image}                        
+                    resizeMode='contain'
+                />
+                <TouchableOpacity
+                    style={[styles.button, { backgroundColor: Colors.tbot.green }]}        
+                    onPress={() => setSettleUpModalShow(true)}
+                    activeOpacity={0.7}
+                >               
+                    <Text style={styles.buttonText}>Settle Up</Text>
+                </TouchableOpacity>
             </View>
-            <ScrollView contentContainerStyle={[tw`flex-grow`, { backgroundColor: Colors.tbot.secondary}]}>
-                <View style={ tw`p-3 `}>
-                    <Text style={tw`text-2xl text-center text-white`}>Total Balance</Text>
-                    <Text style={[tw`text-2xl text-center`, getTextColor(parseFloat(total))]}>{total}</Text>
+            <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+                <View style={styles.balanceContainer}>
+                    <Text style={styles.balanceTitle}>Total Balance</Text>
+                    <Text style={[styles.balanceValue, getTextColor(parseFloat(total))]}>{total}</Text>
                 </View>
-                <View style={{ padding: 5, flexDirection: 'row', justifyContent: 'space-between', borderBottomColor: 'black', borderBottomWidth: 1}}>
+                <View style={styles.balanceDetails}>
                     <View>
-                        <Text style={tw`text-2xl text-center text-white`}>You Owe</Text>
-                        <Text style={[tw`text-2xl text-center`, getTextColor(parseFloat(youOwe))]}>{youOwe}</Text>
+                        <Text style={styles.balanceTitle}>You Owe</Text>
+                        <Text style={[styles.balanceValue, getTextColor(parseFloat(youOwe))]}>{youOwe}</Text>
                     </View>
                     <View>
-                        <Text style={tw`text-2xl text-center text-white`}>Owes You</Text>
-                        <Text style={[tw`text-2xl text-center`, getTextColor(parseFloat(owesYou))]}>{owesYou}</Text>
+                        <Text style={styles.balanceTitle}>Owes You</Text>
+                        <Text style={[styles.balanceValue, getTextColor(parseFloat(owesYou))]}>{owesYou}</Text>
                     </View>
                 </View>
-                <View style={{ padding: 5, flexDirection: 'row', justifyContent: 'space-between' }}>
+                <View style={styles.balancesContainer}>
                     <View>
                         {loggedUser !== "" && renderYouOwe(userBalances)}
                     </View>
@@ -199,5 +206,70 @@ const Dashboard = () => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.tbot.bg,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    button: {
+        borderRadius: 10,
+        minHeight: 62,
+        minWidth: '40%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 8,
+    },
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    image: {
+        width: 40,
+        height: 40,
+    },
+    scrollViewContainer: {
+        flexGrow: 1,
+        backgroundColor: Colors.tbot.secondary,
+    },
+    balanceContainer: {
+        padding: 12,
+    },
+    balanceTitle: {
+        fontSize: 24,
+        textAlign: 'center',
+        color: 'white',
+    },
+    balanceValue: {
+        fontSize: 24,
+        textAlign: 'center',
+    },
+    balanceDetails: {
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+    },
+    balancesContainer: {
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    balanceRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    balanceText: {
+        fontSize: 18,
+        color: 'white',
+        marginRight: 8,
+    },
+});
 
 export default Dashboard;
